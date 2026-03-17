37bd677a1a789562ccf73f90c55b76181ff092ee
Vladimer Gabisonia <vakogabo44@gmail.com>
2026-03-17 18:26:08 +0400

    feat: add global styles, variables and mixins setup

--- online-zoo/src/App.tsx
+++ online-zoo/src/App.tsx
@@ -1,7 +1,19 @@
-import "./App.css";
+import React from "react";
 
-function App() {
-  return <></>;
-}
+import { BrowserRouter, Routes, Route } from "react-router";
+
+import About from "./pages/About/About";
+
+import "./styles/_global.scss";
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
