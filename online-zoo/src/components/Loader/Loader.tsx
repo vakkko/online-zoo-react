c1e5b2c45b1723e104d8569c128faca015fe34ea
Vladimer Gabisonia <vakogabo44@gmail.com>
2026-03-28 14:32:46 +0400

    feat: add Loader component

---
+++ online-zoo/src/components/Loader/Loader.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Loader.scss";
+
+const Loader: React.FC = () => {
+  return <p>Loading...</p>;
+};
+
+export default Loader;
--- online-zoo/src/pages/About/MainContent/PetsCarousel/Carousel/Carousel.tsx
+++ online-zoo/src/pages/About/MainContent/PetsCarousel/Carousel/Carousel.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 import useGetData from "../../../../../utils/useGetData";
+import Loader from "../../../../../components/Loader/Loader";
 
 const Carousel: React.FC = () => {
   const { loading, error, data } = useGetData("pets");
 
-  return <></>;
+  return <>{loading && <Loader />}</>;
 };
 
 export default Carousel;
