229a4d84eff716068118b20fc487adad252e7242
Vladimer Gabisonia <vakogabo44@gmail.com>
2026-04-24 15:32:36 +0400

    feat: add post request for login

--- online-zoo/src/pages/Login/Login.tsx
+++ online-zoo/src/pages/Login/Login.tsx
@@ -1,29 +1,54 @@
 import React from "react";
 
-import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router";
+
+import { useForm, type SubmitHandler } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+
+import axios from "axios";
 
 import { LoginSchema, type LoginSchemaTypes } from "../../schemas/AuthSchema";
 
 import InputAndLabel from "../../components/InputAndLabel/InputAndLabel";
+
+import { BASE_URL } from "../../consts/consts";
 
 import "../../styles/Authorization.scss";
 
 const Login: React.FC = () => {
   const {
     register,
-    formState: { errors },
+    handleSubmit,
+    formState: { errors, isValid },
   } = useForm<LoginSchemaTypes>({
     resolver: yupResolver(LoginSchema),
     mode: "onBlur",
   });
+
+  const navigate = useNavigate();
+
+  const onSubmit: SubmitHandler<LoginSchemaTypes> = (data) => {
+    async function sendData() {
+      try {
+        const response = await axios.post(`${BASE_URL}/auth/login`, data);
+        console.log("response", response);
+        if (response.statusText !== "OK") {
+          throw new Error("Somegin went wrong");
+        }
+        navigate("/");
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    sendData();
+  };
 
   return (
     <main className="authorization-main">
       <div className="registration-container">
         <div className="registration-card">
           <h3>Sign In</h3>
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <InputAndLabel
               label="Log in"
               type="text"
@@ -44,7 +69,7 @@ const Login: React.FC = () => {
               errors={errors}
             />
 
-            <button disabled type="submit">
+            <button disabled={!isValid} type="submit">
               Sign In
             </button>
           </form>
