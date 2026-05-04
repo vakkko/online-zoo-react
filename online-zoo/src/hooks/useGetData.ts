8a883ff223b6d63cd69b79e40b038bd880cdd414
Vladimer Gabisonia <vakogabo44@gmail.com>
2026-05-04 20:40:09 +0400

    refactor: optimize data fetching hook for better error management

--- online-zoo/src/hooks/useGetData.ts
+++ online-zoo/src/hooks/useGetData.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError, CanceledError } from "axios";
 
 import { BASE_URL } from "../consts/consts";
 
@@ -6,27 +6,33 @@ import { useEffect, useState } from "react";
 
 function useGetData<T>(query: string) {
   const [loading, setLoading] = useState<boolean>();
-  const [data, setData] = useState<T[]>();
+  const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState<boolean>();
 
   useEffect(() => {
+    const controller = new AbortController();
     async function fetchData() {
       try {
         setLoading(true);
-        const response = await axios(`${BASE_URL}/${query}`);
-        if (response.status !== 200) {
-          setError(true);
-          return;
-        }
+        const response = await axios(`${BASE_URL}/${query}`, {
+          signal: controller.signal,
+        });
+
         setData(response.data.data);
       } catch (err) {
+        if (err instanceof CanceledError) return;
+
         setError(true);
-        console.error(err);
+        console.error("Fetch Error:", err);
       } finally {
         setLoading(false);
       }
     }
     fetchData();
+
+    return () => {
+      controller.abort();
+    };
   }, [query]);
 
   return { loading, error, data };
